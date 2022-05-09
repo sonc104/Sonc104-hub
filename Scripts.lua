06b06a72e06f528bebc9a89283f77c3ef963c23b
sonc104 <99458131+sonc104@users.noreply.github.com>
2022-05-09 20:43:54 +0700

    Update Scripts.lua

--- Scripts.lua
+++ Scripts.lua
@@ -3,4 +3,6 @@ if game.PlaceId == 9392368856 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/sonc104/just-lord-exploits/main/hack"))()
 elseif game.PlaceId == 7169897329 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/sonc104/sonc104/main/hack"))()
+ elseif PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/sonc104/Sonc104-hub/main/bf"))()
 end
